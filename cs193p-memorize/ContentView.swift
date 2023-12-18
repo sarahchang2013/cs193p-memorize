eb2925f828b3fd9235af81d67886e82f74a2672a
sarahchang2013 <sarah_sarah99@hotmail.com>
2023-12-18 15:47:29 +0800

    CardView toggle content

--- cs193p-memorize/ContentView.swift
+++ cs193p-memorize/ContentView.swift
@@ -8,16 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis:Array<String> = ["👀","🫁","🫀","🧠"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true, content: emojis[0])
+            CardView(isFaceUp: true, content: emojis[1])
+            CardView(isFaceUp: true, content: emojis[2])
+            CardView(isFaceUp: true, content: emojis[3])
         }
+        .foregroundColor(.orange)
         .padding()
+        
     }
 }
+
+struct CardView: View {
+    @State var isFaceUp = false
+    let content: String
+
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.yellow)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+
 
 #Preview {
     ContentView()
