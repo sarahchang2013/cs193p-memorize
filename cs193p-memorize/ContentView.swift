52bd948bcc7fff6ff640537857d62045db38969b
sarahchang2013 <sarah_sarah99@hotmail.com>
2023-12-18 15:22:24 +0800

    Initial Commit

---
+++ cs193p-memorize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cs193p-memorize
+//
+//  Created by sarahchangzsy on 2023/12/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cs193p-memorize/cs193p_memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs193p_memorizeApp.swift
+//  cs193p-memorize
+//
+//  Created by sarahchangzsy on 2023/12/18.
+//
+
+import SwiftUI
+
+@main
+struct cs193p_memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
